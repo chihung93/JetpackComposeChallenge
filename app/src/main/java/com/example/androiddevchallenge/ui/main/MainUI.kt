44e7f5cdb9714b4cb10a81e642af12b5a78c8942
Hung <>
2021-03-23 21:26:07 +0700

    add video and screenshot , clean code

--- app/src/main/java/com/example/androiddevchallenge/ui/main/MainUI.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/main/MainUI.kt
@@ -1,19 +1,18 @@
 package com.example.androiddevchallenge.ui.main
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -23,7 +22,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.theme.*
-import com.example.androiddevchallenge.ui.theme.text_4
 
 @Preview("Dark Theme", widthDp = 375, heightDp = 875)
 @Composable
